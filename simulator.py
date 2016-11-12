7b7846e222291b73f3fb796e5ae5935ab5643424
Amy Shoemaker <amyshoe@stanford.edu>
2016-11-11 19:55:23 -0800

    created simulator file

---
+++ simulator.py
@@ -0,0 +1,11 @@
+# delta_vy_max = 36 km/hr
+# delta_vz_max = 36 km/hr
+# v_y: -50:1:50 km/hr
+# v_z: -20:1:20 km/hr
+# state: (y, z, v_y, v_z, v_w)
+# action: (delta_vy, delta_vz)
+# update: (y + delta_vy +- 0.01(v_w^2), z + delta_vz, v_y + delta_vy +- 0.01(v_w^2), v_z + delta_vz, N(v_w, sigma))
+# +=- 0.01 * v_w(km/hr)^2 <-- effect on v_y
+
+def update(s, a):
+	next_y = s[0] + s[2] +   

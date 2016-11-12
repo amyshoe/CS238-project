78024c19946502b80157b0da37dc88084542d903
Amy Shoemaker <amyshoe@stanford.edu>
2016-11-11 20:27:41 -0800

    created the update function

--- simulator.py
+++ simulator.py
@@ -6,10 +6,47 @@
 # action: (delta_vy, delta_vz)
 # update: (y + delta_vy +- 0.01(v_w^2), z + delta_vz, v_y + delta_vy +- 0.01(v_w^2), v_z + delta_vz, N(v_w, sigma))
 # +=- 0.01 * v_w(km/hr)^2 <-- effect on v_y
+import random
 
 class AirplaneSimulator:
+	'''
+	documentation! we'll do it later
+	'''
+	def __init__(self, input_params):
+		'''
+		fill this in yo
+		'''
+		self.state = None
+		self.sigma = 1
+		self.possible_actions = None
 
-	def __init__(self,):
+		# Initialize class attributes!
+		self.load_params(input_params)
 
-	def update(s, a):
-		next_y = s[0] + s[2] +   
+	def load_params(self, input_params):
+		'''
+		doc string! do it later
+		'''
+		# set the attributes!!!!
+
+	def update(self, action):
+		'''
+		Updates state according to a given action
+		'''
+		if not action in self.possible_actions:
+			print "Action is not possible."
+			return False
+		y = self.state[0] + action[0] + 0.01 * (self.state[4]**2)
+		z = self.state[1] + action[1]
+		v_y = self.state[2] + action[0] + 0.01 * (self.state[4]**2)
+		v_z = self.state[3] + action[1]
+		v_w = random.randn(self.state[4], self.sigma)
+		self.state = (y, z, v_y, v_z, v_w)
+
+	def get_state(self):
+		''' 
+		Returns current state 
+		'''
+		return self.state
+
+
